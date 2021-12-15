69b21a4ff390fa0f416b3f7bd93f48c8bc46b79d
veromarinelli-iram <95386782+veromarinelli-iram@users.noreply.github.com>
2021-12-15 01:02:02 -0300

    Update db-reset.js

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -16,7 +16,7 @@ const USERS_TO_INSERT = [
         "firstName": "Node Goat",
         "lastName": "Admin",
         "password": "Admin_123",
-        //"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
+        "password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
         "isAdmin": true
     }, {
         "_id": 2,
@@ -25,7 +25,7 @@ const USERS_TO_INSERT = [
         "lastName": "Doe",
         "benefitStartDate": "2030-01-10",
         "password": "User1_123"
-        // "password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
+        "password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
     }, {
         "_id": 3,
         "userName": "user2",
@@ -33,7 +33,7 @@ const USERS_TO_INSERT = [
         "lastName": "Smith",
         "benefitStartDate": "2025-11-30",
         "password": "User2_123"
-        //"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
+        "password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
     }];
 
 const tryDropCollection = (db, name) => {
