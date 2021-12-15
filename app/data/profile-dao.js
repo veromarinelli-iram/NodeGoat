99f3eafd9acd50fe6ccefc6039cf95787112b2e6
veromarinelli-iram <95386782+veromarinelli-iram@users.noreply.github.com>
2021-12-14 21:51:04 -0300

    Update profile-dao.js

--- app/data/profile-dao.js
+++ app/data/profile-dao.js
@@ -12,7 +12,7 @@ function ProfileDAO(db) {
 
     const users = db.collection("users");
 
-    /* Fix for A6 - Sensitive Data Exposure
+    // Fix for A6 - Sensitive Data Exposure
 
     // Use crypto module to save sensitive data such as ssn, dob in encrypted format
     const crypto = require("crypto");
@@ -37,7 +37,7 @@ function ProfileDAO(db) {
         const decipher = crypto.createDecipheriv(config.cryptoAlgo, config.cryptoKey, config.iv);
         return `${decipher.update(toDecrypt, "hex", "utf8")} ${decipher.final("utf8")}`;
     };
-    */
+    
 
     this.updateUser = (userId, firstName, lastName, ssn, dob, address, bankAcc, bankRouting, callback) => {
 
@@ -97,12 +97,12 @@ function ProfileDAO(db) {
             },
             (err, user) => {
                 if (err) return callback(err, null);
-                /*
+                
                 // Fix for A6 - Sensitive Data Exposure
                 // Decrypt ssn and DOB values to display to user
                 user.ssn = user.ssn ? decrypt(user.ssn) : "";
                 user.dob = user.dob ? decrypt(user.dob) : "";
-                */
+                
 
                 callback(null, user);
             }
