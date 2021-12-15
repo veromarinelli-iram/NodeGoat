d2630e676c2bc19132387eb604c0c1c840c87856
veromarinelli-iram <95386782+veromarinelli-iram@users.noreply.github.com>
2021-12-14 22:15:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const marked = require("marked");
 const app = express(); // Web framework to handle routing requests
 const routes = require("./app/routes");
 const { port, db, cookieSecret } = require("./config/config"); // Application config properties
-/* 
+
 // Fix for A6-Sensitive Data Exposure
 Load keys for establishing secure HTTPS connection
 const fs = require("fs");
@@ -25,7 +25,7 @@ const httpsOptions = {
     key: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.key")),
     cert: fs.readFileSync(path.resolve(__dirname, "./artifacts/cert/server.crt"))
 };
-*/
+
 
 MongoClient.connect(db, (err, db) => {
     if (err) {
