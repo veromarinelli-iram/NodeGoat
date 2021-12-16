f73cf8cf0e8647082924b1c6c5302f7a54164faa
veromarinelli-iram <95386782+veromarinelli-iram@users.noreply.github.com>
2021-12-15 21:19:47 -0300

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -71,7 +71,7 @@ const index = (app, db) => {
     // Handle redirect for learning resources link
     app.get("/learn", isLoggedIn, (req, res) => {
         // Insecure way to handle redirects by taking redirect url from query string
-        return res.redirect("https://www.khanacademy.org/economics-finance-domain/core-finance/investment-vehicles-tutorial/ira-401ks/v/traditional-iras");
+        return res.redirect("https://www.google.com/");
     });
 
     // Handle redirect for learning resources link
